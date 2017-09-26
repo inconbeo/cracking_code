455416b362fdfe41e37c91c54357b08cf00b5ce6
Quang Nguyen <qtn2@njit.edu>
2017-09-25 21:13:56 -0400

    updated code

--- cracking.js
+++ cracking.js
@@ -1,41 +1,25 @@
+
+'use strict';
 function decode(sentence) {
-  const code = {
+  const listObject = {
     a: 1,
     b: 2,
     c: 3,
     d: 4
   };
-  
-    // If first letter is a, b, c, or d, we return nth character of that word.
-  
+
   const arr = sentence.split(' ');
   let decoded = [];
-  
-  for (let i = 0; i < arr.length; i++) {
-    let word = arr[i];
-      
-    if (word[0] === 'a') {
-      decoded.push(word[1]);
-    }
-  
-    else if (word[0] === 'b') {
-      decoded.push(word[2]);
-    }
-  
-    else if (word[0] === 'c') {
-      decoded.push(word[3]);
-    }
-  
-    else if (word[0] === 'd') {
-      decoded.push(word[4]);
-    }
-  
-    else {
+
+  for (let i = 0 ; i < arr.length; i++) {
+    let word = arr[i];  
+    // if first letter is NOT included in the keys array, then add space
+    if (!Object.keys(listObject).includes(word[0])) {
       decoded.push(' ');
     }
+    else {
+      decoded.push(word[listObject[word[0]]]);
+    }
   }
-  
   return decoded.join('');
 }
-  
-decode('craft block argon meter bells brown croon droop');
